26616538c1ec1d37df7fb2e31955fe13cb2f9e63
wangrumeng <wangrmz202095@163.com>
2023-09-03 10:52:02 +0800

    hystrix之全局服务降级DefaultProperties

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystirxController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystirxController.java
@@ -1,8 +1,9 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.service.PaymentHystrixService;
+
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,29 +16,45 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "paymentInfo_Global_FallbackMethod")
 public class OrderHystirxController {
     @Autowired
-    private PaymentHystrixService  paymentHystrixService;
+    private PaymentHystrixService paymentHystrixService;
 
     @GetMapping("/consumer/payment/hystrix/ok/{id}")
-    public String paymentInfo_OK(@PathVariable("id") Integer id){
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
         return paymentHystrixService.paymentInfo_OK(id);
     }
 
 
-    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutFallbackMethod",commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="1500")
-    })
-    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
-        int age =10 /0;
+
+//    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutFallbackMethod",commandProperties = {
+//            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="1500")
+//    })
+     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+     @HystrixCommand
+     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        int age = 10 / 0;
         return paymentHystrixService.paymentInfo_TimeOut(id);
     }
 
+    /**
+     * fallback和我们业务系统方法混合在一起，耦合度太高了，代码膨胀：
+     * 1、需要通用处理：统一的和自定义的分开
+     * 2、避免代码膨胀
+     *
+     * @param id
+     * @return
+     */
     public String paymentInfo_TimeOutFallbackMethod(Integer id) {
 
-        return "我是消费者80，对方支付系统繁忙请10秒后充实或者自己运行出错请检查自己 o(x_x)o"  ;
+        return "我是消费者80，对方支付系统繁忙请10秒后充实或者自己运行出错请检查自己 o(x_x)o";
     }
 
-
+    /**
+     * 下面是全局fallback
+     */
+    public String paymentInfo_Global_FallbackMethod() {
+        return "Global,对方支付系统繁忙请10秒后充实或者自己运行出错请检查自己 o(x_x)o";
+    }
 }
