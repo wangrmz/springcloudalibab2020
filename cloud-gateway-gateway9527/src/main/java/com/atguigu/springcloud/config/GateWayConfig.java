2ce2f86f5dc334964d3926e7fb27ecca7dc43880
wangrumeng <wangrmz202095@163.com>
2023-09-03 18:37:10 +0800

    gateway服务网关-代码配置路由

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wrmeng
+ * @create 2023-09-03 -18:26
+ **/
+
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customerRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        // http://news.baidu.com/guonei
+        routes.route("path_route_atguigu",
+                r -> r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+}
