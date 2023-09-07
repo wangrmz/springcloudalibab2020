b1f7a70c4be99ec6301f1e646bf3a777ef256f6a
wangrumeng <wangrmz202095@163.com>
2023-09-07 11:18:10 +0800

    Sentinel流控之QPS与线程，快速失败

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/cloudalibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/cloudalibaba/controller/FlowLimitController.java
@@ -3,6 +3,8 @@ package com.atguigu.springcloud.cloudalibaba.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author wrmeng
  * @create 2023-09-07 -10:33
@@ -13,6 +15,11 @@ public class FlowLimitController {
     @GetMapping("/testA")
     public String testA()
     {
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(800);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
         return "------testA";
     }
 
