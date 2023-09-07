e724cf8da73b4c0efeb2acc16fa8f1f32b3953d0
wangrumeng <wangrmz202095@163.com>
2023-09-07 15:13:04 +0800

    Sentinel服务降级规则：RT,异常比例,异常数

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/cloudalibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/cloudalibaba/controller/FlowLimitController.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud.cloudalibaba.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +11,24 @@ import java.util.concurrent.TimeUnit;
  * @create 2023-09-07 -10:33
  **/
 
+/**
+ * Sentinel 提供以下几种熔断策略：
+ * 慢调用比例 (SLOW_REQUEST_RATIO)：选择以慢调用比例作为阈值，需要设置允许的慢调用 RT（即最大的响应时间），请求的响应时间大于该值则统计为慢调用。
+ * 当单位统计时长（statIntervalMs）内请求数目大于设置的最小请求数目，并且慢调用的比例大于阈值，则接下来的熔断时长内请求会自动被熔断。
+ * 经过熔断时长后熔断器会进入探测恢复状态（HALF-OPEN 状态），若接下来的一个请求响应时间小于设置的慢调用 RT 则结束熔断，若大于设置的慢调用 RT 则会再次被熔断。
+ * <p>
+ * 异常比例 (ERROR_RATIO)：当单位统计时长（statIntervalMs）内请求数目大于设置的最小请求数目，并且异常的比例大于阈值，则接下来的熔断时长内请求会自动被熔断。
+ * 经过熔断时长后熔断器会进入探测恢复状态（HALF-OPEN 状态），若接下来的一个请求成功完成（没有错误）则结束熔断，否则会再次被熔断。异常比率的阈值范围是 [0.0, 1.0]，代表 0% - 100%。
+ * <p>
+ * 异常数 (ERROR_COUNT)：当单位统计时长内的异常数目超过阈值之后会自动进行熔断。经过熔断时长后熔断器会进入探测恢复状态（HALF-OPEN 状态），
+ * 若接下来的一个请求成功完成（没有错误）则结束熔断，否则会再次被熔断。
+ */
+
 @RestController
+@Slf4j
 public class FlowLimitController {
     @GetMapping("/testA")
-    public String testA()
-    {
+    public String testA() {
 //        try {
 //            TimeUnit.MILLISECONDS.sleep(800);
 //        } catch (InterruptedException e) {
@@ -24,10 +38,52 @@ public class FlowLimitController {
     }
 
     @GetMapping("/testB")
-    public String testB()
-    {
+    public String testB() {
         return "------testB";
     }
+
+    /**
+     *
+     * ￼异常比例 (ERROR_RATIO)：当单位统计时长（statIntervalMs）内请求数目大于设置的最小请求数目，并且异常的比例大于阈值，则接下来的熔断时长内请求会自动被熔断。
+     * 经过熔断时长后熔断器会进入探测恢复状态（HALF-OPEN 状态），若接下来的一个请求成功完成（没有错误）则结束熔断，否则会再次被熔断。异常比率的阈值范围是 [0.0, 1.0]，代表 0% - 100%。
+     * 两个条件必须同时满足才会进行服务降级，是&&
+     *
+     * @return
+     */
+    @GetMapping("/testD")
+    public String testD() {
+        //暂停几秒钟线程
+//        try {
+//            TimeUnit.SECONDS.sleep(1);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        log.info("testD 测试RT");
+
+        log.info("testD 测试异常比例");
+        int age = 10 / 0;
+
+        return "------testD";
+    }
+
+    /**
+     * 异常数： 异常数是按照分钟统计的 ，大于等于60s
+     *  http://localhost:8401/testE，第一次访问绝对报错，因为除数不能为零，
+     * 我们看到error窗口，但是达到5次报错后，进入熔断后降级。
+     * @return
+     */
+    @GetMapping("/testE")
+    public String testE()
+    {
+        log.info("testE 测试异常比例");
+        int age = 10/0;
+        return "------testE 测试异常比例";
+    }
+
+
+
+
+
 }
 
 
