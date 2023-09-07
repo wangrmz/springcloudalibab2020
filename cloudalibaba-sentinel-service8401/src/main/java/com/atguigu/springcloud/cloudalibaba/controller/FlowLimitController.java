964dfb6e091d18808a6cb7739ac383c011d8077d
wangrumeng <wangrmz202095@163.com>
2023-09-07 10:44:31 +0800

    Sentinel初始化流监控

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/cloudalibaba/controller/FlowLimitController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.cloudalibaba.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wrmeng
+ * @create 2023-09-07 -10:33
+ **/
+
+@RestController
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA()
+    {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB()
+    {
+        return "------testB";
+    }
+}
+
+
