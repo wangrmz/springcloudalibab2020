b1671a189b36283bd31bf42ed275fda1b036ef2d
wangrumeng <wangrmz202095@163.com>
2023-09-07 16:05:38 +0800

    Sentinel-热点key限流

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/cloudalibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/cloudalibaba/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.atguigu.springcloud.cloudalibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -61,7 +64,7 @@ public class FlowLimitController {
 //        log.info("testD 测试RT");
 
         log.info("testD 测试异常比例");
-        int age = 10 / 0;
+        //int age = 10 / 0;
 
         return "------testD";
     }
@@ -76,9 +79,29 @@ public class FlowLimitController {
     public String testE()
     {
         log.info("testE 测试异常比例");
-        int age = 10/0;
+        //int age = 10/0;
         return "------testE 测试异常比例";
     }
+
+    /**
+     * #######################################
+     *    热点key限流
+     *
+     */
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value ="testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2){
+
+        return "-------------testHotKey";
+    }
+
+    public  String deal_testHotKey(String p1, String p2, BlockException exception){
+        return "-------------testHotKey,0(T_T)O"; // 不在使用sentinel默认提示：
+    }
+
+
 
 
 
